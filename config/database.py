b92172ae0e6854446a4d05ff9b63387f31c14332
Sagar Singh Gusain <sagargusain201012@gmail.com>
2026-01-31 16:53:49 +0530

    database connection  mongodb

---
+++ config/database.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGODB_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+print(os.getenv("MONGODB_URI"))
+
+
+client = MongoClient(MONGO_URI)
+
+try:
+    client.admin.command("ping")
+    print("Connected to MongoDB server successfully")
+except Exception as e:
+    print(f"Error connecting to MongoDB server: {e}")
+
+
+#database = client[MONGO_DB_NAME]
+#users_collection = database["users"]
+
