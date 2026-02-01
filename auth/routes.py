c206c58f86b8807183d339c4edb5f923f64a6084
Sagar Singh Gusain <sagargusain201012@gmail.com>
2026-02-02 01:32:05 +0530

    Added login route

--- auth/routes.py
+++ auth/routes.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter , HTTPException ,Depends
 from fastapi import security
 from fastapi.security import HTTPBasic ,HTTPBasicCredentials
 
-from usersmodels import SignUpRequest
-from hashpasswords import hash_password ,verify_password
-from config.database import users_collection
+from .usersmodels import SignUpRequest
+from .hashpasswords import hash_password ,verify_password
+from ..config.database import users_collection
 
 
 router = APIRouter()
@@ -40,7 +40,9 @@ def signup(request:SignUpRequest):
     
 
     
+@router.get("/login")
+def login(user = Depends(authenticate)):
+        return {"message": f"Welcome user {user["username"]}" ,"role":user["role"]}
 
-def login():
-    pass
+    
   
