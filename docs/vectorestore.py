7c4103c0a9d9fcda679215bae9c4a4aaf3c126c5
Sagar Singh Gusain <sagargusain201012@gmail.com>
2026-02-04 00:14:45 +0530

    Pinecone Vector DB integration

--- docs/vectorestore.py
+++ docs/vectorestore.py
@@ -15,8 +15,48 @@ PINECONE_ENV = os.getenv("PINECONE_ENV")
 PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX")
 
 
-os.getenviron["GOOGLE_API_KEY"]= GOOGLE_API_KEY
-
+os.environ["GOOGLE_API_KEY"]= GOOGLE_API_KEY
+#Upload folder setup
 upload_directory ="./uploads"
 #creating a dirctory if it exists than it will not be created again
+## exist_ok=True → prevents error if folder already exists
 os.makedirs(upload_directory, exist_ok=True)
+
+# -----------------------------
+# Initialize Pinecone Client
+# -----------------------------
+
+
+# Create Pinecone client using API key
+pc = Pinecone(api_key = PINECONE_API_KEY)
+
+#Pinecone serverless spec
+
+spec = ServerlessSpec(
+    cloud= 'aws',
+    region='us-east-1'
+)
+
+
+# -----------------------------
+# Check if Index Already Exists
+# -----------------------------
+
+# pc.list_indexes() returns a list of dictionaries
+# Example: [{"name": "medicalassistant"}, {"name": "jobbot"}]
+#Finding for the existing index
+existing_index = [i["name"]for i in pc.list_indexes()]
+
+if PINECONE_INDEX_NAME not in existing_index:
+    pc.create_index(
+                    name =PINECONE_INDEX_NAME,
+                    dimension=768,
+                    metric="dotproduct",
+                    specs= spec
+              )
+              ## Wait until the index is ready
+    while  not pc.describe_index(PINECONE_INDEX_NAME).status["READY"]:
+        time.sleep(1)
+
+# Get a handle to the Pinecone index so you can insert/search vectors
+index = pc.Index(PINECONE_INDEX_NAME)
