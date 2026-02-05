abf5c8ffc037b5e4e4b807b65622a01ec9f76173
Sagar Singh Gusain <sagargusain201012@gmail.com>
2026-02-06 03:14:51 +0530

    Adding upload route in main file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
 from auth.routes import router as auth_router
+from docs.routes import router as docs_router
 app = FastAPI()
 
 app.include_router(auth_router)
-
+app.include_router(docs_router)
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
